bed5413cbdc2da72e993735dabcf6599cbf64e57
hao1988z <gn30837@gmail.com>
2026-08-24 11:55:41 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 window.TRAVEL_JOURNAL_CONFIG = {
   SUPABASE_URL: "https://essrqtxipftrvjzwkpyq.supabase.co",
   SUPABASE_ANON_KEY: "sb_publishable_rGRlyjn-YQgQIbxk5QJwlg_kkj10mwy",
-  PHOTO_BUCKET: "trip-photos"
+  PHOTO_BUCKET: "trip-photos",
+  AUTH_REDIRECT_URL: "https://hao1988z.github.io/my-travel-journal/"
 };
